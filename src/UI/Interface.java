ecdf405040f2ad1b637edec0c27102adddf4ba09
Armand <Armand@130.190.46.243>
2015-05-26 08:54:00 +0200

    Initialisation partie + trie des joueurs par ordre de commencement

--- src/UI/Interface.java
+++ src/UI/Interface.java
@@ -19,6 +19,8 @@ public class Interface {
     
     public void initialiserPartie (){
         Scanner sc = new Scanner(System.in);
+        String[] nomj = new String[6];
+        int[]    nbrj = new int[6];
         int nbrJoueur = 0;
         String nomJoueur;
         while(nbrJoueur<2 | nbrJoueur>6){
@@ -28,7 +30,28 @@ public class Interface {
         for(int i=0; i<nbrJoueur; i++){
             System.out.println("Veuillez saisir le nom du joueur n°"+(i+1)+" : ");
             nomJoueur = sc.nextLine();
-            monopoly.nouveauJoueur(nomJoueur);
+            nomj[i] = nomJoueur;
+            //monopoly.nouveauJoueur(nomJoueur);
+        }
+        
+        // met un lancé de dé pour chaque joueur pour savoir qui commence
+        
+        for(int i=0; i<nbrJoueur; i++){ 
+            nbrj[i] = monopoly.lancerDes();
+        }
+        
+        for(int i=0; i<nbrJoueur; i++){
+            int maxi = 0; //  le nombre max des lancés de dés
+            int jmax = 0; //  le joueur ayant le nombre max
+            for(int j=0; i<nbrJoueur; i++){
+                if (nbrj[i]>maxi) {
+                   maxi = nbrj[j]; 
+                   jmax = j;
+                }
+            }
+            nbrj[jmax] = 0; // met la valeur du lancé du joueur selectionné à 0
+            monopoly.nouveauJoueur(nomj[jmax]); // crée un joueur avec son nom
+        
         }
         
     }
